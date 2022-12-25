eab680cfc6d80b36fd9d663f20ec93b780686ad0
valentina vittoria <valeviit@icloud.com>
2022-12-25 22:08:43 +0100

    boilerplate settings

---
+++ client/src/components/settings/Settings.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div className='container-body'>
+            <h1>Settings</h1>
+            <hr></hr>
+            <div className='containair-box'>
+            <form>
+                <div>
+                <label>Name: </label>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <label>Last Name: </label>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <label>Email:</label>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <label>New Password:</label>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <label>Occupation:</label>
+                <br></br>
+                </div>
+                <div>
+                    <select>
+                        <option value="employee" name="employee">Employee</option>
+                        <option value="manager" name="manager">Manager</option>
+                    </select>
+                </div>
+               
+            </form>
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
