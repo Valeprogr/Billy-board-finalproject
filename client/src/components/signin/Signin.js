013be725fa2d335b58b5d48d9ad2ba3ed9ffbc58
valentina vittoria <valeviit@icloud.com>
2022-12-26 18:47:51 +0100

    edit mongo schema and signin  component

--- client/src/components/signin/Signin.js
+++ client/src/components/signin/Signin.js
@@ -4,24 +4,28 @@ import "../signin/signin.css"
 const Signin = () => {
     return (
         <div id='signin'>
-        <div id='login' className='signin-body'>
-        <div className='signin-box'>
-        <h1>Sign in</h1>
-            <hr></hr>
-            <form>
-                <label for="company-email">Email:</label><br/>
-                <input type="text"></input><br/>
-                <label for="company-password">Password:</label><br/>
-                <input type="text"></input><br/>
+            <div id='login' className='signin-body'>
+                <div className='signin-box'>
+                    <h1>Sign in</h1>
+                    <hr/>
+                    <form>
+                        <label for="name">Name:</label><br />
+                        <input type="text"></input><br />
+                        <label for="lastname">Last Name:</label><br />
+                        <input type="text"></input><br />
+                        <label for="email">Email:</label><br />
+                        <input type="text"></input><br />
+                        <label for="password">Password:</label><br />
+                        <input type="password"></input><br />
+                        <label for="company_name">Company Name:</label><br />
+                        <input type="text" id="company-name" /><br />
+                       
+                        <a href='/login'><h6 className='h6-text'>Do you have already an account ? Click here!</h6></a>
+                        <input type="submit" value="Sign in" className='btn btn-secondary'></input>
+                    </form>
+                </div>
 
-                <label for="company_type">Company type and Description:</label><br/>
-                <input type="text" id="company-type" name="interest"  /><br/>
-                <h6>Do you have already an account ? Click here!</h6>
-                <input type="submit" value="Sign in" className='btn btn-secondary'></input>
-            </form>
-        </div>
-          
-        </div>
+            </div>
         </div>
     );
 }
--- server/models/User.js
+++ server/models/User.js
@@ -5,9 +5,9 @@ const User = new mongoose.Schema({
 "lastname": {type: String, required:true},
 "email": {type: String, required: true},
 "password" : {type: Number, required:true},
-"its_Admin": {type: Boolean, default:false},
+"its_Admin": {type: Boolean, default:true},
 "company_name": {type: String, required:true},
-"user_occupation":{type: String, required:true}
+"user_occupation":{type: String, default: null}
 })
 
 export default mongoose.model("User",User);
