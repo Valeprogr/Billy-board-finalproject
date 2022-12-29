39542fde50e11cbd93b2cabc2e2477b3d57b78bd
valentina vittoria <valeviit@icloud.com>
2022-12-29 19:56:36 +0100

    edit  signin folder position

--- client/src/pages/signin/Signin.js
+++ client/src/pages/signin/Signin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../signin/signin.css"
+import "./signin.css"
 
 const Signin = () => {
     return (
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./pages/home/Navbar";
 import Home from "./pages/home/Home";
-import Signin from "./components/signin/Signin";
+import Signin from "./pages/signin/Signin";
 import Login from './pages/login/Login';
 import About from "./components/about/About";
 import Contacts from "./components/contacts/Contacts";
