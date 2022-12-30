77d84004d0cba917a1307c8712c678e6f69d49df
valentina vittoria <valeviit@icloud.com>
2022-12-30 23:54:21 +0100

    edit signinin

--- client/src/pages/signin/Signin.js
+++ client/src/pages/signin/Signin.js
@@ -4,8 +4,37 @@ import { useHttp } from '../../hooks/http.hook';
 
 import "./signin.css";
 
+import {Btnspinner} from "../../btnSpinner/BtnSpinner";
+import { Toaster } from "react-hot-toast";
 
 const Signin = () => {
+    const {loading, request, error, clearError} =useHttp();
+    const message= useMessage();
+    const [form,setForm]=useState({
+        name:"",
+        lastname:"",
+        email:"",
+        password:"",
+        company_name:""
+    });
+    useEffect(()=>{
+        message(error);
+        clearError();
+    },[error,message,clearError]);
+
+    const changeHandler = event =>{
+        setForm({...form, [event.target.name]: event.target.value});
+    }
+
+    const registerHandler = async (event)=>{
+        event.preventDefault();
+        try{
+            const data = await request("http://localhost:4000/signin","POST", {...form});
+        }catch(error){
+            
+        }
+    }
+
     return (
         <div id='signin'>
             <div id='login' className='signin-body'>
@@ -15,7 +44,7 @@ const Signin = () => {
                     <hr className="horizontalLine"></hr>
                     <form>
                         <label for="name">Name:</label><br />
-                        <input type="text"></input><br />
+                        <input type="text" name='name' onChange={changeHandler} value={form.name}></input><br />
                         <label for="lastname">Last Name:</label><br />
                         <input type="text"></input><br />
                         <label for="email">Email:</label><br />
