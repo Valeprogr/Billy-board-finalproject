ff1ce3bf06c29c5ffce9a0c45d42ead0c4d1e22b
valentina vittoria <valeviit@icloud.com>
2023-01-01 23:29:05 +0100

    edit signin

--- client/src/pages/signin/Signin.js
+++ client/src/pages/signin/Signin.js
@@ -4,7 +4,7 @@ import { useHttp } from '../../hooks/http.hook';
 
 import "./signin.css";
 
-import {Btnspinner} from "../../btnSpinner/BtnSpinner";
+import Btnspinner from "../../btnSpinner/BtnSpinner";
 import { Toaster } from "react-hot-toast";
 
 const Signin = () => {
@@ -45,17 +45,24 @@ const Signin = () => {
                     <form>
                         <label for="name">Name:</label><br />
                         <input type="text" name='name' onChange={changeHandler} value={form.name}></input><br />
+
                         <label for="lastname">Last Name:</label><br />
-                        <input type="text"></input><br />
+                        <input type="text" name="lastname" onChange={changeHandler} value={form.lastname}></input><br />
+
                         <label for="email">Email:</label><br />
-                        <input type="text"></input><br />
+                        <input type="text" name='email' onChange={changeHandler} value={form.email}></input><br />
+
                         <label for="password">Password:</label><br />
-                        <input type="password"></input><br />
+                        <input type="password" name='password' onChange={changeHandler} value={form.password}></input><br />
+
                         <label for="company_name">Company Name:</label><br />
-                        <input type="text" id="company-name" /><br />
+                        <input type="text" id="company-name" name='company_name' onChange={changeHandler} value={form.company_name}/><br />
                        
                         <a href='/login'><h6 className='h6-text'>Do you have already an account ? Click here!</h6></a>
-                        <input type="submit" value="Sign in" className='btn btn-secondary'></input>
+                        <button type="submit" value="Sign in" className='btn btn-secondary' onClick={registerHandler} disabled={loading}>
+                            {loading && <Btnspinner />}
+                            Signin
+                        </button>
                     </form>
                 </div>
                     
