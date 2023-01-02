35bcbb8751a243c2f851d3c810696c7c60cfbf89
valentina vittoria <valeviit@icloud.com>
2023-01-02 16:53:14 +0100

    edit router client side , instal cors,jsonwebtoken

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express,{json} from "express";
 import {} from "dotenv/config";
 import mongoose from "mongoose";
-import userRouter from "./routes/user-router.js"
+import userRouter from "./routes/user-router.js";
+
+import cors from "cors";
 
 const app= express();
 const PORT = process.env.PORT;
 const DB_URI= process.env.DB_URI;
 
-app.use(json());
+app.use(json({ extended : true }));
+
+//use cors
+app.use(cors());
 app.use("", userRouter);
 
 async function StartApp(){
