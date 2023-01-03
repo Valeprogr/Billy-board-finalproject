d4c94aa8a676ea7a79ce11508ed9ada4ac717961
valentina vittoria <valeviit@icloud.com>
2023-01-03 16:57:45 +0100

    edit UserNavbar

--- client/src/components/userHomepage/userHome/NavbarUser.js
+++ client/src/components/userHomepage/userHome/NavbarUser.js
@@ -6,7 +6,7 @@ import "./homeUser.css";
 const NavbarUser = ({ children }) => {
     return (
         <>
-            <nav class="nav flex-column nav-user">
+            <nav className="nav flex-column nav-user">
                 <div className='avatar-img-wow text-center'>
                     <img className='avatar-img' src='https://cdn.discordapp.com/attachments/1008786354865451019/1053355839953588335/profileimage.jpg' alt='profile pictures'></img>
                     <p className='navbar-data'>Mario Rossi</p>
@@ -14,31 +14,31 @@ const NavbarUser = ({ children }) => {
 
                 </div>
                 <br />
-                <NavLink class="nav-link active" aria-current="page" to="/userHome">
+                <NavLink className="nav-link active" aria-current="page" to="/userHome">
                     <span class="material-symbols-outlined">
                         Home
                     </span>
                     <span>Home</span></NavLink >
                 <br />
-                <NavLink class="nav-link" to="user/profile">
+                <NavLink className="nav-link" to="user/profile">
                     <span class="material-symbols-outlined">
                         person
                     </span>
                     Profile</NavLink >
                 <br />
-                <NavLink class="nav-link" to="/employees">
+                <NavLink className="nav-link" to="/employees">
                     <span class="material-symbols-outlined">
                         group
                     </span>
                     Employees</NavLink >
                 <br />
-                <NavLink class="nav-link" to="/projects">
+                <NavLink className="nav-link" to="/projects">
                     <span class="material-symbols-outlined">
                         list_alt
                     </span>
                     Projects</NavLink >
                 <br />
-                <NavLink class="nav-link" to="/user/settings">
+                <NavLink className="nav-link" to="/user/settings">
                     <span class="material-symbols-outlined">
                         settings
                     </span>
