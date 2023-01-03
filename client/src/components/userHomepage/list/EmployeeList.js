285f4423534c7e2625e4e2e9c5b1af0b40c97668
valentina vittoria <valeviit@icloud.com>
2023-01-03 17:43:11 +0100

    edit employeeList

--- client/src/components/userHomepage/list/EmployeeList.js
+++ client/src/components/userHomepage/list/EmployeeList.js
@@ -2,13 +2,55 @@ import React from 'react';
 import data from "../../../data.json"
 
 const EmployeeList = () => {
+    console.log(data)
     return (
         <div className='container-body'>
-            <h2>Employee List</h2>
+            <div className='list-title-emplyeeList'>
+                <h2>Employee List</h2>
+
+                <a href="https://www.w3schools.com">
+                    <span class="material-symbols-outlined">
+                        add_circle
+                    </span>
+                </a>
+            </div>
+
             <hr></hr>
+
             <div className='cotainer-box'>
+                <table class="table">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                            <th>Occupation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{data.users[0].user_name}</td>
+                            <td>{data.users[0].user_lastname}</td>
+                            <td>{data.users[0].user_occupation}</td>
+                            <td>{data.users[0].user_email}</td>
+                        </tr>
+                        <tr>
+                            <td>{data.users[1].user_name}</td>
+                            <td>{data.users[1].user_lastname}</td>
+                            <td>{data.users[1].user_occupation}</td>
+                            <td>{data.users[1].user_email}</td>
+                        </tr>
+                    </tbody>
+                </table>
 
             </div>
+            {/* <button type="button" class="btn btn-secondary">
+
+                <span class="material-symbols-outlined">
+                    add_circle
+                </span>
+                Add Employee
+            </button> */}
         </div>
     );
 }
