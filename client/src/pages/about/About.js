ef92a378df1d025b82ac2a690cc0b7854dc5445d
valentina vittoria <valeviit@icloud.com>
2023-01-06 15:14:40 +0100

    create about page and edit homepage

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./about.css"
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='body-about'>
+            <div className='box-container'>
+                <div className='box'>
+                <h1>About</h1>
+                <hr></hr>
+                <div className='box-text'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br/>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br/>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
 import "./home.css";
+import About from '../about/About';
+import Contacts from '../contacts/Contacts';
+import FooterUser from '../../components/userHomepage/userHome/FooterUser';
 
 
 
 const Home = () => {
     //Hook che serve per navigare su un altra pagina 
     const navigate = useNavigate();
-    
+
     //Funzioni collegate al button signin e login 
     const signinHandler = () => {
         navigate('/signin');
@@ -19,21 +22,22 @@ const Home = () => {
 
     return (
         <>
-           
+
             <div className='home'>
                 <div className='title-btn'>
                     <h1>BILLY</h1>
                     <h1>BOARD</h1>
                     <p>All your work information with you.</p>
                     <div className='btn-container'>
-                    <button type="button" className="btn btn-light" onClick={signinHandler}>Sign in</button>
-                    <button type="button" className="btn btn-light" onClick={loginHandler}>Login</button>
+                        <button type="button" className="btn btn-light" onClick={signinHandler}>Sign in</button>
+                        <button type="button" className="btn btn-light" onClick={loginHandler}>Login</button>
                     </div>
-                  
 
                 </div>
-
             </div>
+                <About />
+                <Contacts />
+                <FooterUser />
         </>
 
     );
