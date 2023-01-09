753fa76dedab0c35bd091b7f282850d8f1fd9afa
valentina vittoria <valeviit@icloud.com>
2023-01-09 16:12:12 +0100

    create ProjectSchema and add route creade Employee

---
+++ server/models/Project.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const Project = new mongoose.Schema({
+    "name": {type: String, required: true},
+    "location": {type:String, required: true},
+    "start_date":{type:String, required:true},
+    "end_date":{type:String, required:true}
+})
+
+export default mongoose.model("Project", Project)
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -52,4 +52,23 @@ router.post("/login", async (req, res) => {
         })
     }
 })
+
+router.post("/createNewEmployee",async (req, res) => {
+    try {
+        const { email, password, name, lastname, company_name, its_Admin, user_occupation } = req.body;
+        const candidate = await User.findOne({ email: email });
+        console.log(candidate);
+
+
+        if (candidate) {
+            return res.status(400).json({ message: "user already exist...", status: "error" });
+        }
+        const user = new User({ name, lastname, email, password: password, company_name, its_Admin, user_occupation });
+        console.log(user);
+        await user.save();
+        res.status(201).json({ message: "user created..." })
+    } catch (error) {
+        res.status(500).json({ message: "error", status: "internal error" });
+    }
+})
 export default router;
