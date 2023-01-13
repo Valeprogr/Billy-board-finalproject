b5e01ac37aceac9af56ba885ca496ac1c61b2c29
valentina vittoria <valeviit@icloud.com>
2023-01-13 20:32:50 +0100

    add  new router on project-router.js

--- server/routes/project-router.js
+++ server/routes/project-router.js
@@ -5,6 +5,9 @@ import ProjectControllers from "../controllers/ProjectControllers.js";
 const router = new Router();
 
 router.post("/CreateProject", ProjectControllers.create);
+router.post("/todolist", ProjectControllers.createTodoList);
+router.get("/todolist", ProjectControllers.getAllTodoList);
+router.post("/todolist/todo", ProjectControllers.createTodo);
 router.get("/projects", ProjectControllers.getAll);
 
 export default router;
