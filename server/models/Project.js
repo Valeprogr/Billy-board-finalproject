eed65da094c6a73bf7d1da986c2fc43888f17213
valentina vittoria <valeviit@icloud.com>
2023-01-13 20:39:43 +0100

    edit  Project Schema

--- server/models/Project.js
+++ server/models/Project.js
@@ -5,7 +5,9 @@ const Project = new mongoose.Schema({
     "location": {type:String, required: true},
     "start_date":{type:String, required:true},
     "end_date":{type:String, required:true},
-    "company_name": {type: String, required:true}
+    "company_name": {type: String, required:true},
+    "todo_list": {type: mongoose.Schema.Types.ObjectId, ref: 'TodoList', default:null},
+    "members":[{type: mongoose.Schema.Types.ObjectId, ref:'User'}]
 })
 
 export default mongoose.model("Project", Project)
