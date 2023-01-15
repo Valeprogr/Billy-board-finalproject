1bfb5a26e3db0b266eccd846d599dabea2f4eace
valentina vittoria <valeviit@icloud.com>
2023-01-15 21:54:07 +0100

    edit style css , create new component

---
+++ client/src/components/userHomepage/empty-projectList/EmptyProjectList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const EmptyProjectList = () => {
+    return (
+        <div style={{position: "fixed", marginTop: "15%",  marginLeft: "30%"}}>
+            <h1 style={{color: "lightgray"}}>No Projects...</h1>
+        </div>
+    );
+}
