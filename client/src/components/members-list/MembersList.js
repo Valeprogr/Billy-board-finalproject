f78e0b80b03b369cb8b7ad1d9e4f7a85681e462a
valentina vittoria <valeviit@icloud.com>
2023-01-15 21:56:51 +0100

    create component member list

---
+++ client/src/components/members-list/MembersList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const MembersList = ({ project }) => {
+    return (
+        <div className="px-0 border-top">
+            <div className="card border-0" style={{ width: "auto" }}>
+                <div className="card-body">
+                    <button className="btn btn-outline-primary">add Member</button>
+                    
+                </div>
+            </div>
+
+        </div>
+    );
+}
