94f7a9fe317266390fcebcec23f43cb51123d8a1
valentina vittoria <valeviit@icloud.com>
2023-01-16 20:50:12 +0100

    edit tasklist

--- client/src/components/userHomepage/task/Tasklist.js
+++ client/src/components/userHomepage/task/Tasklist.js
@@ -1,36 +1,35 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Task from './Task';
-import {useHttp} from "../../../hooks/http.hook";
+import { useHttp } from "../../../hooks/http.hook";
 import CreateNewTask from './CreateNewTask';
 //import CreateNewTask from './CreateNewTask';
 
 
-const REACT_APP_URL_CYCLIC=process.env.REACT_APP_URL_CYCLIC;
-const Tasklist = ({projects}) => {
+const REACT_APP_URL_CYCLIC = process.env.REACT_APP_URL_CYCLIC;
+const Tasklist = ({ projects }) => {
     //console.log(projects)
-    const{request}=useHttp();
-    const[tasks, setTask]=useState([]);
+    const { request } = useHttp();
+    const [tasks, setTask] = useState([]);
     const [createTask, setCreateTask] = useState(false);
-    
-    useEffect(()=>{
-        const getTask = async ()=>{
-            const data = await request(`${REACT_APP_URL_CYCLIC}todos`,'POST', { "todoList_id": projects.todo_list });
-            setTask(prev=>[...data]);
+
+    useEffect(() => {
+        const getTask = async () => {
+            const data = await request(`${REACT_APP_URL_CYCLIC}todos`, 'POST', { "todoList_id": projects.todo_list });
+            setTask(prev => [...data]);
         }
         getTask();
-    },[])
+    }, [])
     //console.log(tasks)
     return (
         <>
             <div>
-            <h4>Task list</h4>
-            {tasks.length !==0 ?
-            (tasks.map(task=> <Task task={task} key={task._id}/>))
-            :
-            null}
-                {/* <Task projects={projects} /> */}
+                <h4>Task list</h4>
+                {tasks.length !== 0 ?
+                    (tasks.map(task => <Task task={task} key={task._id} />))
+                    :
+                    null}
                 <button className="btn btn-outline-secondary" onClick={() => setCreateTask(prev => !prev)}>Create New Task</button>
-                    {createTask && <CreateNewTask project={projects} />}
+                {createTask && <CreateNewTask project={projects} />}
             </div>
         </>
     );
