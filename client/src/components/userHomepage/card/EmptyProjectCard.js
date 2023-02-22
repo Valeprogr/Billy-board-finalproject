4251a5f52c1b477a769cbe0d0c6ccae151cfd3ea
valentina vittoria <valeviit@icloud.com>
2023-02-22 20:47:04 +0100

    create emptyProj.Card & add a new functions

---
+++ client/src/components/userHomepage/card/EmptyProjectCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./cardProjects.css"
+
+const EmptyProjectCard = () => {
+    return (
+        <div className='body-holiday-card'>
+            <div className='title-container'>
+            <p>No current Project ...</p>
+            </div>
+        </div>
+    );
+}
+
+export default EmptyProjectCard;
--- client/src/components/userHomepage/userHome/UserHome.js
+++ client/src/components/userHomepage/userHome/UserHome.js
@@ -1,44 +1,57 @@
 import React, { useState, useEffect,useContext } from 'react';
 import FooterUser from './FooterUser';
 import "../userHome/homeUser.css";
-import Calender from '../calender/Calender';
 import HeaderUserHome from '../header-userHome/HeaderUserHome';
 import { useHttp } from '../../../hooks/http.hook';
 import CardProjects from '../card/CardProjects';
 import { AuthContext } from '../../../context/AuthContext';
-
+import EmptyProjectCard from '../card/EmptyProjectCard';
 
 const REACT_APP_URL_CYCLIC = process.env.REACT_APP_URL_CYCLIC;
 //const REACT_APP_LOCALHOST =process.env.REACT_APP_LOCALHOST;
 const UserHome = () => {
     const { request } = useHttp();
     const [currentProj, setCurrentProj] = useState([]);
-    const auth=useContext(AuthContext);
+    const [showComponent, setShowComponent] = useState(false);
+    const auth = useContext(AuthContext);
    
 
         useEffect(() => {
         const getProj = async () => {
             const proj = await request(`${REACT_APP_URL_CYCLIC}project/current/${auth.company}`);
-            console.log(proj)
-            setCurrentProj(prev => [...proj]);
+            setCurrentProj(prev => [...proj]); 
+            console.log(currentProj)
         }
         getProj();
     }, [])
 
+    useEffect(()=>{
+        if(currentProj !== []){
+            setShowComponent(true)
+        }
+    },[])
+        
     return (
 
         <div className='admin-home-body'>
             <HeaderUserHome userId={auth.userId}/>
 
             <div className='admin-home-container box-card'>
-                {currentProj !== null ?
-                    <div className="position-relative container">
-                        <h3 className="card-title text-secondary">Your Current Project</h3>
+            <h3 className="card-title text-secondary">Your Current Project</h3>
+                { !showComponent ?
+                    <EmptyProjectCard />
+                    :
+                    <>
+                     {currentProj !== null ?
+                    <div className="container">
                         {currentProj.map(p => <CardProjects index={p} projects={p} />)}
                     </div>
                     :
-                    null}
-                <Calender />
+                   null}    
+                    </>
+                }
+               
+                
             </div>
 
 
