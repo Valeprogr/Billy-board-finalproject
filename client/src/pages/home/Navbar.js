06d51456b068ec3d5b990db688582020ec7087aa
valentina vittoria <valeviit@icloud.com>
2023-03-03 01:57:12 +0100

    edit some css errors responsive

--- client/src/pages/home/Navbar.js
+++ client/src/pages/home/Navbar.js
@@ -8,7 +8,7 @@ import logo from "../../images/logo.png";
 const Navbar = ({children}) => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-body">
         <NavLink className="navbar-brand" to="/">
         <img className='logo' src={logo} alt="billy-board-logo"/>
         </NavLink>
@@ -16,14 +16,14 @@ const Navbar = ({children}) => {
           <span className="navbar-toggler-icon">
           </span>
         </button>
-        <div className="collapse navbar-collapse navbar-body " id="navbarSupportedContent">
+        <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             
-              <div className="navbar-nav">
+              <div className="navbar-nav nav-list">
                 <NavLink className="nav-item nav-link active" to="/">Home</NavLink>
                 <NavLink className="nav-item nav-link" to="/about">About</NavLink>
                 <NavLink className="nav-item nav-link" to="/contacts">Contacts</NavLink>
               </div>
-              <div>
+              <div className='containar-btn-login'>
               <NavLink className="nav-item nav-link btn btn-secondary btn-login" to="/login">Login</NavLink>
               </div>
            
