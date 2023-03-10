794df37575d43ebbebaf1cf88744bd13ff615d21
valentina vittoria <valeviit@icloud.com>
2023-03-11 00:40:21 +0100

    edit navbar error

--- client/src/components/userHomepage/card/CardEmployee.js
+++ client/src/components/userHomepage/card/CardEmployee.js
@@ -7,7 +7,7 @@ const CardEmployee = ({employee}) => {
     return (
         <div>
             <div className="card-employee" >
-                <img className='card-img' src="https://cdn.discordapp.com/attachments/1008786354865451019/1053355839953588335/profileimage.jpg" class="card-img-top" alt="..." />
+                <img className='card-img employee-card-img' src="https://cdn.discordapp.com/attachments/1008786354865451019/1053355839953588335/profileimage.jpg" class="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{employee.name} {employee.lastname}</h5>
                     <p className="card-text">{employee.user_occupation}</p>
